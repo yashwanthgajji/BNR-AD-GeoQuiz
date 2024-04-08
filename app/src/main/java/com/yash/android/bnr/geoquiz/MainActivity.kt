ae34865c29da9ede0f73e976559d8eaedbabd6d6
Yashwanth Gajji <yashwanth.gajji333@gmail.com>
2024-04-08 19:47:20 -0400

    add-new-questions

--- app/src/main/java/com/yash/android/bnr/geoquiz/MainActivity.kt
+++ app/src/main/java/com/yash/android/bnr/geoquiz/MainActivity.kt
@@ -10,6 +10,16 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
